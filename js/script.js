cb44ea2acf7a151fdf035fd5bf6c36b2dabfaf24
AimanFadil <aimanbello2001@gmail.com>
2023-11-30 14:27:02 +0100

    funzioni creacella e nuovogioco

--- js/script.js
+++ js/script.js
@@ -1,7 +1,23 @@
 //Griglia Campo Minato
 
+function creaCella(num, RigaCelle){
+    const quadrato = document.createElement('div')
+    quadrato.classList.add('quadrato')
+    
+    return quadrato;
+}
+
+
+
+function creaNuovoGioco(){
+    const griglia = document.getElementById('griglia')
+
+    griglia.innerHTML = ''
+    
+}
+
 const bottoneGioco = document.getElementById('gioca')
 
 bottoneGioco.addEventListener('click', function(){
-    
+    creaNuovoGioco()
 }); 
