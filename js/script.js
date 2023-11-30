56ea187e9181091dbd04621f3d9e0e7159f68bd9
AimanFadil <aimanbello2001@gmail.com>
2023-11-30 14:12:38 +0100

    correzione html e css

--- js/script.js
+++ js/script.js
@@ -1,22 +1,2 @@
 //Griglia Campo Minato
-function CreaGriglia(){
-        document.getElementById('griglia').innerHTML = '';
-}
-   
 
-function generaGriglia() {
-    let griglia = "";
-    for (let i = 1; i <= 100; i++) {
-        griglia += "<button class='cella '>" + i + "</button>";
-        if (i % 10 === 0) {
-            griglia += "<br>";
-        }
-    }
-    return griglia;
-}
-
-document.getElementById("griglia").innerHTML = generaGriglia();
-
-document.getElementById('gioca').addEventListener('click', function(){
-    CreaGriglia();
-})
