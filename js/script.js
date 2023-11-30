b582f5e94eecbc949ed8f7db286ab38f43009b2e
AimanFadil <aimanbello2001@gmail.com>
2023-11-30 15:01:03 +0100

    bonus e super bonus

--- js/script.js
+++ js/script.js
@@ -1,5 +1,4 @@
 //Griglia Campo Minato
-
 function creaCella(num, RigaCelle){
     const quadrato = document.createElement('div')
     quadrato.classList.add('quadrato')
@@ -12,7 +11,10 @@ function CreaGriglia(numeroCelle, RigaCelle){
     for(let i=1; i<=numeroCelle; i++){
         let cella = creaCella(i, RigaCelle);
 
-        
+        cella.addEventListener('click', function(){
+            this.classList.add('clicked');
+            console.log(this.innerText);
+        });
 
         griglia.appendChild(cella);
     }
@@ -22,13 +24,37 @@ function CreaGriglia(numeroCelle, RigaCelle){
 function creaNuovoGioco(){
     const griglia = document.getElementById('griglia')
 
-    
+    const difficoltà = document.getElementById('difficoltà')
+    let livello = parseInt(difficoltà.value);
+
+    let numeroCelle;
+    let NumCellePerRiga;
+
+    switch(livello){
+        case 1:
+            numeroCelle = 100;
+            break;
+        case 2:
+            numeroCelle = 81;
+            break;
+        case 3:
+            numeroCelle = 49;
+            break;
+        default:
+            break;
+    }
+
+    NumCellePerRiga = Math.sqrt(numeroCelle);
 
     griglia.innerHTML = ''
     CreaGriglia(numeroCelle, NumCellePerRiga);
     for(let i=1; i<=100; i++){
         let cella = creaCella(i);
 
+        cella.addEventListener('click', function(){
+            this.classList.add('evidenziato');
+            console.log(this.innerText);
+        });
 
         griglia.appendChild(cella);
     }
