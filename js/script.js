a913e1b6dfdea97b4aa4d6e8569063e4d0019bc8
AimanFadil <aimanbello2001@gmail.com>
2023-11-30 14:43:21 +0100

    aggiunte proprietà e valori alle funzioni

--- js/script.js
+++ js/script.js
@@ -3,29 +3,39 @@
 function creaCella(num, RigaCelle){
     const quadrato = document.createElement('div')
     quadrato.classList.add('quadrato')
-    
+    quadrato.style.width = `calc(100% /${RigaCelle})`
+    quadrato.innerText = num;
     return quadrato;
 }
 
-function CreaGriglia(){
+function CreaGriglia(numeroCelle, RigaCelle){
     for(let i=1; i<=numeroCelle; i++){
         let cella = creaCella(i, RigaCelle);
+
+        
 
         griglia.appendChild(cella);
     }
 }
 
 
-
 function creaNuovoGioco(){
     const griglia = document.getElementById('griglia')
 
-    griglia.innerHTML = ''
     
+
+    griglia.innerHTML = ''
+    CreaGriglia(numeroCelle, NumCellePerRiga);
+    for(let i=1; i<=100; i++){
+        let cella = creaCella(i);
+
+
+        griglia.appendChild(cella);
+    }
 }
 
 const bottoneGioco = document.getElementById('gioca')
 
 bottoneGioco.addEventListener('click', function(){
-    creaNuovoGioco()
+    creaNuovoGioco();
 }); 
